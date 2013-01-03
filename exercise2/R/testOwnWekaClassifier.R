9d909ffdc7985a48e01ce381d8a0928c1bb53341
Robert Schaefer <robert.schaefer@student.hpi.uni-potsdam.de>
2013-01-03 15:10:13 +0100

    reorderd R-scripts to own folder

--- exercise2/R/testOwnWekaClassifier.R
+++ exercise2/R/testOwnWekaClassifier.R
@@ -1,7 +1,7 @@
 #!/usr/bin/Rscript
 require("RWeka")
 require("rJava")
-.jaddClassPath("./target/machine_learning-0.0.1-SNAPSHOT.jar")
+.jaddClassPath("../BayesianNetwork/target/machine_learning-0.0.1-SNAPSHOT.jar")
 
 classifier <- make_Weka_classifier("at/ac/tuwien/machine_learning/SillyClassifier")
 classifier 
