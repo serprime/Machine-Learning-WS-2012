318404a569fe365ebb8922416eee33667b4ebbe5
Robert Schaefer <robert.schaefer@student.hpi.uni-potsdam.de>
2013-01-03 15:19:45 +0100

    bayesNetTest.R now uses our own UltraSearcher which is nothing more than a copy of HillClimber (at the moment!)

--- exercise2/R/bayesNetTest.R
+++ exercise2/R/bayesNetTest.R
@@ -1,9 +1,12 @@
 #!/usr/bin/Rscript
 require("RWeka")
+require("rJava")
+
+.jaddClassPath("../BayesianNetwork/target/machine_learning-0.0.1-SNAPSHOT.jar")
 
 bayesNet <- make_Weka_classifier("weka/classifiers/bayes/BayesNet")
 
-res = bayesNet(Species~., data=iris, control = Weka_control(D="",  Q = "weka.classifiers.bayes.net.search.local.HillClimber", "--", P=1, S="BAYES", E="weka.classifiers.bayes.net.estimate.SimpleEstimator", "--", A=0.5))
+res = bayesNet(Species~., data=iris, control = Weka_control(D="",  Q = "at.ac.tuwien.machine_learning.UltraSearcher", "--", P=1, S="BAYES", E="weka.classifiers.bayes.net.estimate.SimpleEstimator", "--", A=0.5))
 predict(res)
 
 
