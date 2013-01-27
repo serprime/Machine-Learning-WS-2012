c6fcdeeb003fda40f21ca7866a957e9d8066bbae
philipp steinwender <philipp.a.steinwender@gmail.com>
2013-01-27 21:34:48 +0100

    wooot

--- exercise3/knn-java-gui/run.sh
+++ exercise3/knn-java-gui/run.sh
@@ -1,1 +1,1 @@
-java -cp out/production/KNN:lib/uncommons-maths-1.2.3.jar:lib/weka.jar:lib/weka-src.jar at.ac.tuwien.knn.gui.MainWindow
+java -cp bin:lib/uncommons-maths-1.2.3.jar:lib/weka.jar at.ac.tuwien.knn.gui.MainWindow
