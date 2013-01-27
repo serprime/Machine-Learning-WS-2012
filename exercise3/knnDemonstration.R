6e9a0cf0463e4a641568d2879f88751285387f70
Robert Schaefer <robert.schaefer@student.hpi.uni-potsdam.de>
2013-01-27 11:28:36 +0100

    refactoring of test/train data schema test added short version of parameter 'e'

--- exercise3/knnDemonstration.R
+++ exercise3/knnDemonstration.R
@@ -8,7 +8,7 @@ option_list <- list(
                     make_option(c("-T", "--test"), help="load test data, if ommitted, training data is splitted into parts", metavar="ARFF_FILE"),
                     make_option(c("-k", "--kneighbors"), type="integer", help="k parameter for nearest neighbor classifier", default=as.integer(5), metavar="K"),
                     make_option(c("-c", "--color"), type="character", default="Prediction", help="color the points based on the predicted value or based on the prediction correctness", metavar="Prediction|Correctness"),
-                    make_option(c("--extract"), type="integer", help="reduce input data set by extracting every nth instance", metavar="N")
+                    make_option(c("-e","--extract"), type="integer", help="reduce input data set by extracting every nth instance", metavar="N")
                     )
 parser <- OptionParser(usage="knnDemonstration [option] trainingfile", option_list=option_list)
 arguments <- parse_args(parser, positional_arguments = TRUE)
@@ -77,10 +77,8 @@ if( ncol(testing) != ncol(training)){
     stop(sprintf("The number of columns for the provided data sets is not the same: %s columns for trainingdata and %s columns for testdata.",  ncol(training), ncol(testing)))
 }
 
-for (i in 1:ncol(training)){
-    if (names(testing)[i] != names(training)[i]){
-        stop(sprintf("Please ensure that both training set and test data set have the same schema"))
-        }
+if (!all(names(testing) == names(training))){
+    stop(sprintf("Please ensure that both training set and test data set have the same schema.\nThe training set looks like: %s \nAnd the test data looks like %s", paste(names(training),"",collapse=""),paste(names(testing),"",collapse="")))
 }
 
 if (modus == "2D"){
@@ -101,7 +99,7 @@ if (modus == "2D"){
             testing$Color <- testing$P
         } else if (opt$color == "Correctness"){
             testing$Color <- (testing$Class == testing$P)
-        }
+        } # TODO: does another switch makes sense? Maybe for 'Class', the true Class Attribute without prediction?
         # TODO: maybe output some evaluation data? e.g. percentage of misclassified instances
         if (modus == "3D"){
             plot3d(testing[[1]], testing[[2]], testing[[3]], xlab=names(testing)[1], ylab=names(testing)[2], zlab=names(testing)[3], col=testing$Color, size=1, type='s')
